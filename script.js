1fe956466d4d060e646b30e05d7edeceb53a8306
vnaazleen <shaikvaseemnaazleen@gmail.com>
2021-12-08 19:18:25 +0530

    Complete Assignment

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+
+    var year = parseInt(new Date().getFullYear()) + parseInt(t)
+
+    var A = p * r * t;
+    var result = `If you deposit ${p},<br>
+    at an interest rate of ${r}%.<br>
+    You will recieve an amount of ${A}.<br>
+    in the year ${year}
+    `
+    document.getElementById('result').innerHTML = result;
 }
-        
+
+var selectYears = document.getElementById("years");
+for (let i = 1; i <= 100; i++)
+selectYears.innerHTML += "<option>" + i + "</option>";
+
+document.getElementById('rate').onchange=function(){
+    document.getElementById('value').innerHTML = this.value + "%";
+};
